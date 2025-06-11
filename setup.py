5cea4371a174ac1f87ab75d3dc216a0cbf2f3e26
Ninjakid07 <86473886+Ninjakid07@users.noreply.github.com>
2025-06-11 17:43:28 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/tree_navigation_launch.py']),
+        # Removed launch file reference since it doesn't exist yet
     ],
     install_requires=[
         'setuptools',
